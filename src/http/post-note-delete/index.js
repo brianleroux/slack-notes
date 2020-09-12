f980f4e478e91e98c7c85781831dfaf5c0f605df
Brian LeRoux <b@brian.io>
2020-09-11 17:47:53 -0700

    Update index.js

--- src/http/post-note-delete/index.js
+++ src/http/post-note-delete/index.js
@@ -2,7 +2,7 @@ let arc = require('@architect/functions')
 
 exports.handler = arc.http.async(handler)
 
-async function (req) {
+async function handler (req) {
   console.log(req)
   return { ok: true }
 }
