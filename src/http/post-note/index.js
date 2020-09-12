cf1cf81ee231c0cbe6f5ad7c7119f903f86816c5
Brian LeRoux <b@brian.io>
2020-09-11 17:01:11 -0700

    Update index.js

--- src/http/post-note/index.js
+++ src/http/post-note/index.js
@@ -1,7 +1,17 @@
-let qs = require('querystring')
+let arc = require('@architect/functions')
+let data = require('@begin/data')
+
+exports.handler = arc.http.async(handler)
+
+async function handler(req) {
+  return { body: "cool, cool, got it " + '```' + JSON.stringify(req.body) + '```' }
+}
+
+/*let qs = require('querystring')
 
 exports.handler = async function note(req) {
   let decoded = Buffer.from(req.body, 'base64').toString()
   let json = qs.parse(decoded)
   return "cool, got it " + '```' + JSON.stringify(json) + '```'
 }
+*/
