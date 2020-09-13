b35eccaa4eaa4dab8d02192b3bf5b697ae88df9a
Brian LeRoux <b@brian.io>
2020-09-12 18:57:59 -0700

    Update index.js

--- src/http/post-note-delete/index.js
+++ src/http/post-note-delete/index.js
@@ -1,9 +1,25 @@
 let arc = require('@architect/functions')
+let data = require('@begin/data')
+let render = require('@architect/shared/note')
 
 exports.handler = arc.http.async(handler)
 
 async function handler (req) {
+  
   let raw = JSON.parse(req.body.payload)
-  console.log(raw)
-  return { ok: true }
+  let user_id = raw.user.id
+  let team_id = raw.user.team_id
+  let table = `notes-${ team_id }-${ user_id }`
+  let key = raw.actions[0].value
+  
+  // nuke the note
+  await data.destroy({table, key})
+  
+  // read the notes back
+  let notes = await data.get({ table })
+  let blocks = notes.map(render)
+  
+  return {
+    json: { blocks }
+  }
 }
